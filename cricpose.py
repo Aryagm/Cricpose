fd25f714182edcc64ce66e0c552e72d539953692
Arya Manjaramkar <80019072+Aryagm@users.noreply.github.com>
2021-10-04 16:42:27 -0400

    Update cricpose.py

--- cricpose.py
+++ cricpose.py
@@ -5,72 +5,100 @@ from pynput.keyboard import Key, Controller
 import webview
 
 cap = cv2.VideoCapture(0)
-def shot(window):   
+
+
+def shot():
     keyboard = Controller()
     mp_pose = mp.solutions.pose
 
-    def calculate_angle(a,b,c):
-        a = np.array(a) # First
-        b = np.array(b) # Mid
-        c = np.array(c) # End
-        
-        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-        angle = np.abs(radians*180.0/np.pi)
-        
-        if angle >180.0:
-            angle = 360-angle
-            
+    def calculate_angle(a, b, c):
+        a = np.array(a)  # First
+        b = np.array(b)  # Mid
+        c = np.array(c)  # End
+
+        radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(
+            a[1] - b[1], a[0] - b[0]
+        )
+        angle = np.abs(radians * 180.0 / np.pi)
+
+        if angle > 180.0:
+            angle = 360 - angle
+
         return angle
 
     lst = []
     lst = lst[-500:]
     ## Setup mediapipe instance
-    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5, model_complexity=0, enable_segmentation=False, static_image_mode=False) as pose:
+    with mp_pose.Pose(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5,
+        model_complexity=0,
+        enable_segmentation=False,
+        static_image_mode=False,
+    ) as pose:
         while cap.isOpened():
             _, frame = cap.read()
-            
+
             # Recolor image to RGB
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             image.flags.writeable = False
-        
+
             # Make detection
             results = pose.process(image)
-        
+
             # Recolor back to BGR
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            
+
             # Extract landmarks
             try:
                 landmarks = results.pose_landmarks.landmark
-                
+
                 # Get coordinates
-                shoulder_l = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-                elbow_l = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-                wrist_l = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-                
+                shoulder_l = [
+                    landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y,
+                ]
+                elbow_l = [
+                    landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y,
+                ]
+                wrist_l = [
+                    landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y,
+                ]
+
                 # Get coordinates
-                shoulder_r = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-                elbow_r = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-                wrist_r = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-                
+                shoulder_r = [
+                    landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                    landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y,
+                ]
+                elbow_r = [
+                    landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                    landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y,
+                ]
+                wrist_r = [
+                    landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                    landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y,
+                ]
+
                 # Calculate angle
                 angle_l = calculate_angle(shoulder_l, elbow_l, wrist_l)
                 angle_r = calculate_angle(shoulder_r, elbow_r, wrist_r)
-                
+
                 if angle_l > 140 or angle_r > 120 and True not in lst:
                     lst.append(True)
                     keyboard.press(Key.enter)
                     keyboard.release(Key.enter)
                 else:
                     lst.append(False)
-                        
+
             except:
                 pass
 
-if __name__ == '__main__':
-    window = webview.create_window('Cricket!', 'https://doodlecricket.github.io/#/')
-    webview.start(shot, window)
+
+if __name__ == "__main__":
+    webview.create_window("Cricket!", "https://doodlecricket.github.io/#/")
+    webview.start(shot)
     cap.release()
     cv2.destroyAllWindows()
-    
